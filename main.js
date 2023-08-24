a2ff91cf49b3da6716acc1a610977ee856c55d6d
João Carlos Borges <136731149+Joao-carlos-zimba@users.noreply.github.com>
2023-08-24 19:42:57 -0300

    Commit JS 2
    Links das abas

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
-const saibaMaisButtons = document.querySelectorAll('.saiba-mais');
-console.log(saibaMaisButtons)
+window.onload = function() {
+    var hash = window.location.hash;
+    var tabContent = document.querySelectorAll('.tabela');
+    console.log(hash)
 
-saibaMaisButtons.forEach(button => {
-    button.addEventListener('click', (event)=> {
-        //evita que o link redirecione imediatamente
+for (var i = 0; i < tabContent.length;i++) {
+    tabContent[i].classList.remove('active');
+}
 
-        //obtém o valor do atributo-target
-        const targetTabContentld = button.getAttribute('data-bs-target');
-        localStorage.setltem('targetTabContentld', targetTabContentld);
-        console.log(targetTabContentld)
-    });
-});
+if (hash) {
+    document.querySelector(hash).classList.add('active');
+    document.querySelector(hash+'1').classList.add('active');
+    console.log(hash)
+    console.log(hash+'1')
+
+} else {
+    tabContent[0].classList.add('active');
+}
+};
+
+
+// const saibaMaisButtons = document.querySelectorAll('.saiba-mais');
+// console.log(saibaMaisButtons)
+
+// saibaMaisButtons.forEach(button => {
+//     button.addEventListener('click', (event)=> {
+//          //evita que o link redirecione imediatamente
+
+//          //obtém o valor do atributo-target
+//         const targetTabContentld = button.getAttribute('data-bs-target');
+//         localStorage.setltem('targetTabContentld', targetTabContentld);
+//         console.log(targetTabContentld)
+//     });
+// });
